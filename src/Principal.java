aed6586bc301c6d2d687baac824144e99c93a02d
josemoncada87 <josemoncada87@hotmail.com>
2020-11-13 16:54:44 -0500

    initial commit

---
+++ src/Principal.java
@@ -0,0 +1,47 @@
+import processing.core.PApplet;
+
+public class Principal extends PApplet {
+
+	public static void main(String[] args) {
+		PApplet.main("Principal");
+	}
+	
+	@Override
+	public void settings() {
+		size(400, 400);
+	}
+
+		
+	@Override
+	public void setup() {
+
+	}
+
+	@Override
+	public void draw() {
+
+	}		
+	
+	@Override
+	public void keyPressed() {		
+		
+	}
+	
+	@Override
+	public void mousePressed() {		
+		
+	}
+	
+	@Override
+	public void mouseDragged() {
+		
+	}
+	
+	@Override
+	public void mouseReleased() {
+	}
+}
+
+
+	
+
