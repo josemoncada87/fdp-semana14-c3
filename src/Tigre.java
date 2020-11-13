135823bf514ee2eab094156dad5098370af922f5
josemoncada87 <josemoncada87@hotmail.com>
2020-11-13 17:04:51 -0500

    del equipo

--- src/Tigre.java
+++ src/Tigre.java
@@ -1,4 +1,28 @@
 
 public class Tigre {
 
+	private int x, y;
+	
+	public Tigre() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	
+
 }
